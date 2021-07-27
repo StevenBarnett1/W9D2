f2faed88e8b700ff073517d11226a2a22ea68c43
Stevenbarnett1 <Stevenbarnett1@live.com>
2021-07-27 10:33:02 -0700

    first done

--- first/assets/js/html-collection.js
+++ first/assets/js/html-collection.js
@@ -1,11 +1,12 @@
 export default () => {
   const bodyChildElements = document.body.children; // HTMLCollection [div]
-  // console.log(bodyChildElements);
+  console.log("html collection",bodyChildElements);
 
   const div = bodyChildElements[0];
-
+  console.log("html collection 0th",div)
   const divChildElements = div.children; // HTMLCollection [span]
   const helloWorld = div.innerText; // Hello World! Yes!    <-- NOT Hello World!
   const span = divChildElements[0]; // <span>Yes!</span>
-  // debugger
+
+  
 };
